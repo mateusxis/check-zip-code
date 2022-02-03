ab5b0540574db60da5d289770be7e78ae296b5fe
Mateus Barbosa <mateusx@poli.ufrj.br>
2022-02-03 01:21:27 -0300

    [container] add logger in container

--- src/container.js
+++ src/container.js
@@ -2,11 +2,13 @@ const { createContainer, asFunction, asValue } = require('awilix');
 const router = require('./application/router');
 const server = require('./application/server');
 const config = require('./application/config');
+const loggerFactory = require('./infrastruture/logger');
 
 const container = createContainer();
 
 container.register({
   config: asValue(config),
+  logger: asFunction(loggerFactory).singleton(),
   router: asFunction(router).singleton(),
   server: asFunction(server).singleton()
 });
