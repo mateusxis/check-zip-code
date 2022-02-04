9420bc588836bb101f8c7920822252576289c51f
Mateus Barbosa <mateusx@poli.ufrj.br>
2022-02-04 01:34:14 -0300

    [container] add zipCodeDomain and searchZipCodeService

--- src/container.js
+++ src/container.js
@@ -1,16 +1,20 @@
 const { createContainer, asFunction, asValue } = require('awilix');
+const config = require('./config');
 const router = require('./application/router');
 const server = require('./application/server');
-const config = require('./application/config');
+const zipCodeDomain = require('./domain/zipCode');
 const loggerFactory = require('./infrastructure/logger');
+const searchZipCodeService = require('./integrations/searchZipCodeService');
 
 const container = createContainer();
 
 container.register({
-  config: asValue(config),
+  config: asValue(config()),
   logger: asFunction(loggerFactory).singleton(),
   router: asFunction(router).singleton(),
-  server: asFunction(server).singleton()
+  server: asFunction(server).singleton(),
+  searchZipCodeService: asFunction(searchZipCodeService).singleton(),
+  zipCodeDomain: asFunction(zipCodeDomain).singleton()
 });
 
 module.exports = container;
