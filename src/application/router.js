5a5f485de947ebef16ec6a75595a054eb9d8d584
Mateus Barbosa <mateusx@poli.ufrj.br>
2022-02-04 01:34:45 -0300

    [router] create route readiness and address

--- src/application/router.js
+++ src/application/router.js
@@ -1,12 +1,36 @@
-const { OK } = require('http-status');
+const { OK, INTERNAL_SERVER_ERROR, NOT_FOUND } = require('http-status');
 const Router = require('koa-router');
 
-module.exports = () => {
+module.exports = ({ logger, zipCodeDomain }) => {
   const router = new Router();
+
+  router.get('/address', async (ctx) => {
+    const { zipCode } = ctx.query;
+
+    try {
+      const address = await zipCodeDomain.get({
+        zipCode: zipCode
+      });
+
+      ctx.body = address;
+      ctx.status = address ? OK : NOT_FOUND;
+    } catch (error) {
+      logger.error({ error }, 'Error getting address.');
+      ctx.body = error.massage;
+      ctx.status = INTERNAL_SERVER_ERROR;
+    }
+  });
 
   router.get('/healthcheck', (ctx) => {
     ctx.status = OK;
     ctx.body = 'OK';
+  });
+
+  router.get('/readiness', async (ctx) => {
+    const zipCodeLiveness = await zipCodeDomain.isUp();
+
+    ctx.status = zipCodeLiveness ? OK : INTERNAL_SERVER_ERROR;
+    ctx.body = zipCodeLiveness ? 'OK' : 'NOK';
   });
 
   return router;
